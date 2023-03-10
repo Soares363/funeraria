0f66e6c99bf23e7a51379f62579176ff4f327638
LucasAltino <124930759+LucasAltino@users.noreply.github.com>
2023-03-10 10:46:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,16 @@
 <?php 
 
-include("Animal.php");
-include("Cachorro.php");
-include("Gato.php");
-include("Tartaruga.php");
-include("Cobra.php");
+include("Produtos.php");
+include("Adicionais.php");
 
 
-$cao = new Cachorro("Totó", "Vira-lata", 3, "Caramelo");
-$gato = new Gato("Mingau", "Vira-lata", 2, "Branco");
-$tartaruga = new Tartaruga("Tartaruga", "Jabuti", 100, "Verde");
-$cobra = new Cobra("Leticia", "Python", 5, "Preto");
+$produtos = new Produtos("Caixão", 200, "Madeira de Carvalho");
+$produto2 = new Produtos("Coroa de Flores", 100, "Coroa de Rosas");
+$produto3 = new Produtos("Velas", 20, "Velas Brancas");
+$produto4 = new Produtos("Capela", 120, "Capela Jaspe");
 
-echo "Nome: " . $cobra->getNome() . " <br>";
-echo "Raça: " . $cobra->getRaca() . " <br>";
-echo "Idade: " . $cobra->getIdade() . " <br>";
-echo "Cor: " . $cobra->getCor() . " <br>";
-$cobra->falar();
-$cobra->andar();
+echo "Nome: " . $produtos->getNome() . " <br>";
+echo "Valor: " . $produtos->getValor() . " <br>";
+echo "Descrição: " . $produtos->getDescricao() . " <br>";
 
 echo "<hr>";
-
-echo "Nome: " . $cao->getNome() . " <br>";
-echo "Raça: " . $cao->getRaca() . " <br>";
-echo "Idade: " . $cao->getIdade() . " <br>";
-echo "Cor: " . $cao->getCor() . " <br>";
-$cao->falar();
-$cao->andar();
-
-echo "<hr>";
-
-echo "Nome: " . $gato->getNome() . " <br>";
-echo "Raça: " . $gato->getRaca() . " <br>";
-echo "Idade: " . $gato->getIdade() . " <br>";
-echo "Cor: " . $gato->getCor() . " <br>";
-$gato->falar();
-
-echo "<hr>";
-
-echo "Nome: " . $tartaruga->getNome() . " <br>";
-echo "Raça: " . $tartaruga->getRaca() . " <br>";
-echo "Idade: " . $tartaruga->getIdade() . " <br>";
-echo "Cor: " . $tartaruga->getCor() . " <br>";
-$tartaruga->falar();
